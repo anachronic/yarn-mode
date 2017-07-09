74f00e0e5ddb0da851b062f5e554315b160812a9
Nicolas Salas V <nikosalas@gmail.com>
2017-07-09 11:29:02 -0400

    Fontify package names and add autoload cookies

--- yarn-mode.el
+++ yarn-mode.el
@@ -2,17 +2,36 @@
 ;;; Commentary:
 ;;; Code:
 
-(defvar yarn-mode-syntax-table nil "Syntax table for `yarn-mode'.")
 
-(defvar yarn-mode-keywords-re
-  (regexp-opt '("version" "resolved" "dependencies"))
+(defvar yarn-mode-syntax-table
+  nil
+  "Syntax table for `yarn-mode'.")
+
+(defvar yarn-mode-attributes-re
+  nil
   "Keywords of yarn-mode.")
 
-(defvar yarn-mode-font-lock-defaults nil
+(defvar yarn-mode-font-lock-defaults
+  nil
   "List for font lock in `yarn-mode'.")
 
+(defvar yarn-mode-package-re
+  nil
+  "Regular expression that defines a package.")
+
+(defvar yarn-mode-dependencies-re
+  nil
+  "Regular expression that defines a package dependency.")
+
+(setq yarn-mode-package-re "\\(^\\|,\\s-\\)\\([a-zA-Z-_0-9]+\\)@")
+(setq yarn-mode-dependencies-re "\\s-\\{4,\\}\\([a-zA-Z-_0-9]+\\)\\s-")
+(setq yarn-mode-attributes-re (regexp-opt '("version" "resolved" "dependencies")))
+
 (setq yarn-mode-font-lock-defaults
-      `(,yarn-mode-keywords-re . (1 font-lock-builtin-face)))
+      `((,yarn-mode-attributes-re . font-lock-builtin-face)
+        (,yarn-mode-package-re . (2 font-lock-keyword-face t)) ;; Direct deps
+        (,yarn-mode-dependencies-re . (1 font-lock-keyword-face t)) ;; Dep of another dep (nested)
+        ))
 
 (setq yarn-mode-syntax-table
       (let ((syntable (make-syntax-table)))
@@ -21,10 +40,14 @@
         (modify-syntax-entry ?\" "\"" syntable)
         syntable))
 
+;;;###autoload
 (define-derived-mode yarn-mode text-mode "Yarn"
   "Simple mode to highlight yarn.lock files."
   :syntax-table yarn-mode-syntax-table
   (setq font-lock-defaults '(yarn-mode-font-lock-defaults)))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("yarn\\.lock\\'" . yarn-mode))
 
 (provide 'yarn-mode)
 ;;; yarn-mode.el ends here
